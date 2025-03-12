98242c414dfb4b307c4c1c3b527b78575e295768
RianJP2006 <22402032@bucks.ac.uk>
2025-03-12 17:49:14 +0000

    I have updated the movement, gravity, animation and jump features

--- src/characters/Gangster.py
+++ src/characters/Gangster.py
@@ -1,57 +1,91 @@
 import pygame
 import os
-
+ 
 class ShooterCharacter(pygame.sprite.Sprite): #This class is a subcla
-	def __init__(self,character_type,x, y, scale,speed):
-		pygame.sprite.Sprite.__init__(self)
-		#Character attributes
-		self.character_type = character_type
-		self.speed = speed
-		self.direction = int(1)
-		self.flip = bool(False)
-		self.alive = bool(True)
-		self.velocity_y = float(0)
-		self.jump = bool(False)
-		self.in_air = bool(False)
-		self.animation_list = []
-		self.frame_index = int(0)
-		self.action = int(0)
-		self.update_time = pygame.time.get_ticks()
-		
-		#animation configuration
-		animations = ['Idle','Run','Jump']
-		for animation in animations:
-			list = []
-			no_frames = len(os.listdir(f'images/{self.character_type}/{animation}'))
-			for i in range(no_frames):
-				animation_img = pygame.image.load(f'imgages/{self.character_type}/{animation}/{i}.png')
-				animation_img = pygame.transform.scale(animation_img, (int(animation_img.get_width() * scale), int(animation_img.get_height() * scale)))
-				list.append(animation_img)
-			self.animation_list.append(list)
-
-
-		self.image = self.animation_list[self.action][self.frame_index]
-		self.rect = self.image.get_rect()
-		self.rect.center = (x, y)
-	
-	def draw(self, window):
-		window.blit(pygame.transform.flip(self.image,self.flip,False), self.rect) 	
-	
-
-	def move (self,left,right):
-			
-		dx = int(0)
-		dy = int(0)
-		if left:
-			dx = -self.speed
-			self.flip = True
-			self.direction = -1
-		if right:
-			dx = self.speed
-			self.flip = False
-			self.direction = 1
-		self.rect.x += dx
-		self.rect.y += dy
-
-
-		
+    def __init__(self,character_type,x, y, scale,speed):
+        pygame.sprite.Sprite.__init__(self)
+        #Character attributes
+        self.character_type = character_type
+        self.speed = speed
+        self.direction = int(1)
+        self.flip = bool(False)
+        self.alive = bool(True)
+        self.vel_y = float(0)
+        self.jump = bool(False)
+        self.in_air = bool(False)
+        self.animation_list = []
+        self.frame_index = int(0)
+        self.action = int(0)
+        self.update_time = pygame.time.get_ticks()
+       
+        #animation configuration
+        animations = ['Idle','Run','Jump']
+        for animation in animations:
+            list = []
+            no_frames = len(os.listdir(f'images/{self.character_type}/{animation}'))
+            for i in range(no_frames):
+                animation_img = pygame.image.load(f'images/{self.character_type}/{animation}/{i}.png')
+                animation_img = pygame.transform.scale(animation_img, (int(animation_img.get_width() * scale), int(animation_img.get_height() * scale)))
+                list.append(animation_img)
+            self.animation_list.append(list)
+ 
+ 
+        self.image = self.animation_list[self.action][self.frame_index]
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
+ 
+    def move (self,left,right):
+        GRAVITY = float(0.75)
+        dx = int(0)
+        dy = int(0)
+ 
+        if left:
+            dx = -self.speed
+            self.flip = True
+            self.direction = -1
+ 
+        if right:
+            dx = self.speed
+            self.flip = False
+            self.direction = 1
+ 
+        if self.jump and self.in_air == False:
+            self.vel_y = -11
+            self.jump = False
+            self.in_air = True
+ 
+        self.vel_y += GRAVITY
+        if self.vel_y > 10:
+            self.vel_y
+        dy += self.vel_y
+ 
+        if self.rect.bottom + dy > 300:
+            dy = 300 - self.rect.bottom
+            self.in_air = False
+ 
+        self.rect.x += dx
+        self.rect.y += dy
+ 
+   
+    def animation(self):
+        COOLDOWN = 100
+ 
+        self.image = self.animation_list[self.action][self.frame_index]
+        if pygame.time.get_ticks() - self.update_time > COOLDOWN:
+            self.update_time = pygame.time.get_ticks()
+            self.frame_index += 1
+        if self.frame_index >= len(self.animation_list[self.action]):
+            self.frame_index = 0
+ 
+ 
+ 
+    def update_action(self, new_action):
+        if new_action != self.action:
+            self.action = new_action
+            self.frame_index = 0
+            self.update_time = pygame.time.get_ticks()
+ 
+ 
+ 
+    def draw(self,window):
+        window.blit(pygame.transform.flip(self.image, self.flip, False), self.rect)
--- src/game.py
+++ src/game.py
@@ -1,47 +1,58 @@
 import pygame
 from characters.Gangster import *
-
+ 
 pygame.init()
-
+ 
 WINDOW_WIDTH = int(800)
 WINDOW_HEIGHT = int(WINDOW_WIDTH * 0.8)
-
+ 
 window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption('Game*')
-
+ 
 #Frames per second
 game_clock = pygame.time.Clock()
 FPS = float(60)
-
+ 
 #Player actions
 left = bool(False)
 right = bool(False)
-
+ 
 #Game Variables
-gravity = float(0.75)
-
+GRAVITY = float(0.75)
+ 
 #colours
 bgColour = tuple((100,200,130))
 RedColour = tuple((255,0,0))
-
+ 
 def draw_background():
     window.fill(bgColour)
-
-
+ 
+ 
 GameRunning = True
-
-Gangster = ShooterCharacter('gangster',200, 200, 0.1,5)
-Police = ShooterCharacter('enemy',400, 200, 0.1,5)
-
+ 
+Gangster = ShooterCharacter('gangster',200, 200, 1,2)
+Police = ShooterCharacter('gangster',400, 200, 1,2)
+ 
 while GameRunning:
     game_clock.tick(FPS)
     draw_background()
-
+ 
+    Gangster.animation()
     Gangster.draw(window)
     Police.draw(window)
-
+ 
     Gangster.move(left,right)
-
+ 
+    if Gangster.alive:
+        if Gangster.in_air:
+            Gangster.update_action(2)  # 2: jump
+        elif left or right:
+            Gangster.update_action(1)  # 1: run
+        else:
+            Gangster.update_action(0)  # 0: idle
+        Gangster.move(left, right)
+ 
+ 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             GameRunning = False
@@ -50,15 +61,20 @@ while GameRunning:
                 left = True
             if event.key == pygame.K_d:
                 right = True
+            if event.key == pygame.K_w and Gangster.alive:
+                Gangster.jump = True
             if event.key == pygame.K_ESCAPE:
                 GameRunning = False
+ 
+ 
+ 
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_a:
                 left = False
             if event.key == pygame.K_d:
                 right = False
      
-
+ 
     pygame.display.update()
-
+ 
 pygame.quit()
